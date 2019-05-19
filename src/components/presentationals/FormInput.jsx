98a2ac15e9bcb4d97b87bb2fb1016b39bd67e19c
Emmanuel Daniel <emmanuel.daniel@andela.com>
2019-05-20 00:05:52 +0100

    created form component

--- src/components/presentationals/FormInput.jsx
+++ src/components/presentationals/FormInput.jsx
@@ -3,29 +3,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const FormInput = props => {
+  const { id, className, title, name, placeholder, defaultValue } = props;
   return (
     <div className="field">
       <input
-        className={props.input.class}
-        name={props.input.name}
-        id={props.input.id}
-        value={props.input.value}
-        placeholder={props.input.placeholder || ''}
+        {...props}
+        className={className}
+        name={name}
+        id={id || name}
+        defaultValue={defaultValue}
+        placeholder={placeholder || title}
       />
-      <label htmlFor={props.input.id}>{props.input.title}:</label>
+      <label htmlFor={id || name}>{title}:</label>
     </div>
   );
 };
 
 FormInput.propTypes = {
-  input: PropTypes.shape({
-    name: PropTypes.string,
-    class: PropTypes.string,
-    id: PropTypes.string,
-    title: PropTypes.string,
-    value: PropTypes.string,
-    placeholder: PropTypes.string,
-  }),
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  defaultValue: PropTypes.string,
+  placeholder: PropTypes.string,
 };
 
 export default FormInput;
