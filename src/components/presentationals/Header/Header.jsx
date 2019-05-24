337f7d96387437509a17607b19e6759a179db903
Emmanuel Daniel <emmanuel.daniel@andela.com>
2019-05-24 12:47:47 +0100

    tested image component

--- src/components/presentationals/Header/Header.jsx
+++ src/components/presentationals/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Image from '../Image';
+import Image from '../Image/Image';
 import logo from '../../assets/images/official/logo.png';
 import NavBar from '../NavBar';
 
---
+++ src/components/presentationals/Image/Image.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Image = ({ src, alt, className, style }) => {
+  return (
+    <img
+      src={src}
+      className={className || ''}
+      alt={alt || src}
+      style={style || ''}
+    />
+  );
+};
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  alt: PropTypes.string,
+  style: PropTypes.string,
+};
+
+export default Image;
