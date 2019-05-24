8b55ae89a9304820ae7449658e08b886d219e354
Emmanuel Daniel <emmanuel.daniel@andela.com>
2019-05-24 11:53:20 +0100

    fixed javascript enabled pages

--- src/Index.jsx
+++ src/Index.jsx
@@ -8,7 +8,9 @@ import Footer from './components/presentationals/Footer';
 import Loader from './components/presentationals/Loader';
 import ToastBar from './components/presentationals/ToastBar';
 import FormInput from './components/presentationals/FormInput';
+import FormButton from './components/presentationals/FormButton';
 import Modal from './components/presentationals/Modal';
+import Form from './components/presentationals/Form';
 
 const footer = {
   poweredby: {
@@ -17,11 +19,29 @@ const footer = {
   },
   url: 'http://emmsdan.com/php',
 };
+const form = {
+  id: 'form',
+  className: '',
+  button: {
+    className: 'red',
+    text: 'delete',
+    onClick: () =>
+      alert("ToastBar toast={{ class: 'show', message: 'deleted' }} />"),
+  },
+};
 const input = {
-  id: 'address',
-  class: '',
   title: 'enter address',
   name: 'address',
+  className: 'red',
+  id: 'delete',
+  onClick: () =>
+    alert("ToastBar toast={{ class: 'show', message: 'deleted' }} />"),
+};
+const button = {
+  ...input,
+  text: 'register',
+  className: 'blue',
+  type: 'reset',
 };
 const model = {
   state: 'flex',
@@ -39,18 +59,21 @@ const model = {
 
 ReactDOM.render(
   <BrowserRouter>
-    <Loader loader="hide" />
+    <Loader loader="show" />
     <Header />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+    
     <ToastBar message="I am a toastbar" className="show" />
-    <FormInput input={input} />
+    <Form {...form}>
+      <FormInput {...input} />
+      <FormInput
+        name="email"
+        type="email"
+        title="email reset"
+        className="blue-gray"
+        defaultValue="ecomje@fmail.com"
+      />
+      <FormButton {...button} />
+    </Form>
     <Modal {...model} />
     <Footer footer={footer} />
   </BrowserRouter>,
